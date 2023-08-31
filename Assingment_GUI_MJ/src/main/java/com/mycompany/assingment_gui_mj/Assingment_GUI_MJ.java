fdcae7a4337cd2de25b2598e28e431f54f2d919c
Kasaje <bowieparkour@gmail.com>
2023-08-31 16:06:25 +0700

    First Commit

---
+++ Assingment_GUI_MJ/src/main/java/com/mycompany/assingment_gui_mj/Assingment_GUI_MJ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assingment_gui_mj;
+
+/**
+ *
+ * @author hp
+ */
+public class Assingment_GUI_MJ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
